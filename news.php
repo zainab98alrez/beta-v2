cf1303c1d17153d3badf714661a34b990126aa3b
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-07-06 22:34:25 +0300

    fix issues

--- news.php
+++ news.php
@@ -6,26 +6,26 @@
 
   <!-- loader -->
   <?php include 'include/loader.php'; ?>
-<?php include 'include/menu.php'; ?>
-    
-      <div class="intro-section" style="background-image: url('images/home/1.jpg');">
-      <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
-              <h1>News BETA Logistics</h1>
-              <p> 
-                    BETA LOGISTICS SPECIALIZES IN PROVIDING EXPERT INTERNATIONAL OCEAN FREIGHT FORWARDING 
-                    SERVICES TO COMPANIES LARGE AND SMALL.
-               </p>
-              <p><a href="/contact" class="btn-beta btn btn-primary py-3 px-5">Contact</a></p>
-            </div>
-          </div>
+  <?php include 'include/menu.php'; ?>
+
+  <div class="intro-section site-blocks-cover innerpage d-flex justify-content-center align-items-center" style="background-image: url('images/home/1.jpg');">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
+          <h1>News BETA Logistics</h1>
+          <p>
+            BETA LOGISTICS SPECIALIZES IN PROVIDING EXPERT INTERNATIONAL OCEAN FREIGHT FORWARDING
+            SERVICES TO COMPANIES LARGE AND SMALL.
+          </p>
+          <p><a href="/contact" class="btn-beta btn btn-primary py-3 px-5">Contact</a></p>
         </div>
       </div>
+    </div>
+  </div>
 
-      </div>
+  </div>
 
-    
+
   <!-- news section -->
   <div class="site-section bg-light">
     <div class="container">
@@ -50,7 +50,7 @@
               </div>
             </div>
           </a>
-          </div>
+        </div>
         <div class="col-md-6 col-lg-4 mb-4">
           <a href="/news">
             <div class="service-39381">
@@ -67,19 +67,19 @@
           </a>
         </div>
         <div class="col-md-6 col-lg-4 mb-4">
-           <a href="/news">
-              <div class="service-39381">
-                <img src="images/home/2.webp" alt="Image" class="img-fluid">
-                <div class="p-4">
-                    <h3><a href="#">New event in Croatia Columbia</a></h3>
-                    <div class="d-flex">
-                    <div class="mr-auto">
-                        <p class="mb-1 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    </div>
+          <a href="/news">
+            <div class="service-39381">
+              <img src="images/home/2.webp" alt="Image" class="img-fluid">
+              <div class="p-4">
+                <h3><a href="#">New event in Croatia Columbia</a></h3>
+                <div class="d-flex">
+                  <div class="mr-auto">
+                    <p class="mb-1 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
                 </div>
               </div>
-           </a>
+            </div>
+          </a>
         </div>
       </div>
       <div class="row">
@@ -97,7 +97,7 @@
               </div>
             </div>
           </a>
-          </div>
+        </div>
         <div class="col-md-6 col-lg-4 mb-4">
           <a href="/news">
             <div class="service-39381">
@@ -114,48 +114,48 @@
           </a>
         </div>
         <div class="col-md-6 col-lg-4 mb-4">
-           <a href="/news">
-              <div class="service-39381">
-                <img src="images/home/2.webp" alt="Image" class="img-fluid">
-                <div class="p-4">
-                    <h3><a href="#">New event in Croatia Columbia</a></h3>
-                    <div class="d-flex">
-                    <div class="mr-auto">
-                        <p class="mb-1 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-                    </div>
+          <a href="/news">
+            <div class="service-39381">
+              <img src="images/home/2.webp" alt="Image" class="img-fluid">
+              <div class="p-4">
+                <h3><a href="#">New event in Croatia Columbia</a></h3>
+                <div class="d-flex">
+                  <div class="mr-auto">
+                    <p class="mb-1 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
                 </div>
               </div>
-           </a>
+            </div>
+          </a>
         </div>
       </div>
     </div>
   </div>
   <!-- end news section -->
 
-    
-   <!-- get in touch -->
-   <div class="site-section bg-image overlay" style="background-image: url('images/hero_2.jpg');">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-7 text-center">
-            <h2 class="text-black mb-3">Get In Touch With Us</h2>
-            <p class="lead text-black mb-5">BETA LOGISTICS OFFERS ITS 
-                TECHNICALLY COMPETENT AND DEDICATED TEAMS TO 
-                SUPPORT THE EVER-GROWING NEED OF CUSTOMERS’ 
-                TRANSPORTING AND SHIPPING VALUABLE AND HIGHLY-PRICED DEVICES. 
-            </p>
-            <p class="mb-0"><a href="/contact" class="btn-beta py-3 px-5">Contact Us</a></p>
-          </div>
+
+  <!-- get in touch -->
+  <div class="site-section bg-image overlay" style="background-image: url('images/hero_2.jpg');">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-7 text-center">
+          <h2 class="text-black mb-3">Get In Touch With Us</h2>
+          <p class="lead text-black mb-5">BETA LOGISTICS OFFERS ITS
+            TECHNICALLY COMPETENT AND DEDICATED TEAMS TO
+            SUPPORT THE EVER-GROWING NEED OF CUSTOMERS’
+            TRANSPORTING AND SHIPPING VALUABLE AND HIGHLY-PRICED DEVICES.
+          </p>
+          <p class="mb-0"><a href="/contact" class="btn-beta py-3 px-5">Contact Us</a></p>
         </div>
       </div>
     </div>
-<!-- end get in touch -->
+  </div>
+  <!-- end get in touch -->
 
 
-<!-- footer -->
-<?php include 'include/footer.php'; ?>
-    
+  <!-- footer -->
+  <?php include 'include/footer.php'; ?>
+
 
 </body>
 
--- services.php
+++ services.php
@@ -7,169 +7,109 @@
   <!-- loader -->
   <?php include 'include/loader.php'; ?>
 
-    <?php include 'include/menu.php'; ?>
-    
-    <div class="intro-section" style="background-image: url('images/home/1.jpg');">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
-              <h1>Services BETA Logistics</h1>
-              <p> 
-                    BETA LOGISTICS SPECIALIZES IN PROVIDING EXPERT INTERNATIONAL OCEAN FREIGHT FORWARDING 
-                    SERVICES TO COMPANIES LARGE AND SMALL.
-               </p>
-              <p><a href="/contact" class="btn-beta btn btn-primary py-3 px-5">Contact</a></p>
-            </div>
-          </div>
+  <?php include 'include/menu.php'; ?>
+
+  <div class="intro-section site-blocks-cover innerpage d-flex justify-content-center align-items-center" style="background-image: url('images/home/1.jpg');">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7 mx-auto text-center" data-aos="fade-up">
+          <h1>Services BETA Logistics</h1>
+          <p>
+            BETA LOGISTICS SPECIALIZES IN PROVIDING EXPERT INTERNATIONAL OCEAN FREIGHT FORWARDING
+            SERVICES TO COMPANIES LARGE AND SMALL.
+          </p>
+          <p><a href="/contact" class="btn-beta btn btn-primary py-3 px-5">Contact</a></p>
         </div>
       </div>
+    </div>
+  </div>
 
-    
 
-    <div class="py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-yacht"></span>
-              </span>
-              <h3>Luxuries Yacht</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-shield"></span>
-              </span>
-              <h3>30 Years of Experience</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-captain"></span>
-              </span>
-              <h3>Good Captain</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
 
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-devices"></span>
-              </span>
-              <h3>Luxuries Yacht</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-yacht"></span>
+            </span>
+            <h3>Luxuries Yacht</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
           </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-badge"></span>
-              </span>
-              <h3>30 Years of Experience</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-shield"></span>
+            </span>
+            <h3>30 Years of Experience</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
           </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-captain-1"></span>
-              </span>
-              <h3>Good Captain</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-captain"></span>
+            </span>
+            <h3>Good Captain</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-devices"></span>
+            </span>
+            <h3>Luxuries Yacht</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-badge"></span>
+            </span>
+            <h3>30 Years of Experience</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mb-5">
+          <div class="service-29283">
+            <span class="wrap-icon-39293">
+              <span class="flaticon-captain-1"></span>
+            </span>
+            <h3>Good Captain</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    
-    <div class="site-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center">
-            <span class="text-serif text-primary">Testimonial</span>
-            <h3 class="heading-92913 text-black text-center">What Customer Saying...</h3>
-          </div>
-        </div>
-        <div class="row">
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_3_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_4_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_3_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
+
+
   <!-- get in touch -->
   <div class="site-section bg-image overlay" style="background-image: url('images/hero_2.jpg');">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-7 text-center">
-            <h2 class="text-black mb-3">Get In Touch With Us</h2>
-            <p class="lead text-black mb-5">BETA LOGISTICS OFFERS ITS 
-                TECHNICALLY COMPETENT AND DEDICATED TEAMS TO 
-                SUPPORT THE EVER-GROWING NEED OF CUSTOMERS’ 
-                TRANSPORTING AND SHIPPING VALUABLE AND HIGHLY-PRICED DEVICES. 
-            </p>
-            <p class="mb-0"><a href="/contact" class="btn-beta py-3 px-5">Contact Us</a></p>
-          </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-7 text-center">
+          <h2 class="text-black mb-3">Get In Touch With Us</h2>
+          <p class="lead text-black mb-5">BETA LOGISTICS OFFERS ITS
+            TECHNICALLY COMPETENT AND DEDICATED TEAMS TO
+            SUPPORT THE EVER-GROWING NEED OF CUSTOMERS’
+            TRANSPORTING AND SHIPPING VALUABLE AND HIGHLY-PRICED DEVICES.
+          </p>
+          <p class="mb-0"><a href="/contact" class="btn-beta py-3 px-5">Contact Us</a></p>
         </div>
       </div>
     </div>
-<!-- end get in touch -->
-    
-<!-- footer -->
-<?php include 'include/footer.php'; ?>
+  </div>
+  <!-- end get in touch -->
+
+  <!-- footer -->
+  <?php include 'include/footer.php'; ?>
 
 </body>
 
